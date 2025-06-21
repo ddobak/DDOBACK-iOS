66a9f29d2bf661e2d0dd598c4d82f711f7d5ad14
GeonWoo Lee <2dubu.dev@gmail.com>
2025-06-21 15:07:30 +0900

    프로젝트 기초 설정

---
+++ DDOBAK/DDOBAK/DDOBAKApp.swift
@@ -0,0 +1,17 @@
+//
+//  DDOBAKApp.swift
+//  DDOBAK
+//
+//  Created by 이건우 on 6/21/25.
+//
+
+import SwiftUI
+
+@main
+struct DDOBAKApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
