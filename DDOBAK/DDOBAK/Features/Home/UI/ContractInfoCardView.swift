03410e33f3c78e439386cb0e347a065d244632d1
GeonWoo Lee <2dubu.dev@gmail.com>
2025-07-10 01:07:26 +0900

    공통 컴포넌트 폴더명 변경

---
+++ DDOBAK/DDOBAK/Features/Home/UI/ContractInfoCardView.swift
@@ -0,0 +1,18 @@
+//
+//  ContractInfoCardView.swift
+//  DDOBAK
+//
+//  Created by 이건우 on 7/10/25.
+//
+
+import SwiftUI
+
+struct ContractInfoCardView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ContractInfoCardView()
+}
