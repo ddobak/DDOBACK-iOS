d567b3448b99ba60afe8b6a602a48fc47223faba
GeonWoo Lee <2dubu.dev@gmail.com>
2025-07-20 19:41:40 +0900

    TipCardView 개발

--- DDOBAK/DDOBAK/Data/DTO/Tip/Tip.swift
+++ DDOBAK/DDOBAK/Data/DTO/Tip/Tip.swift
@@ -7,7 +7,7 @@
 
 import Foundation
 
-struct Tip: Decodable {
+struct Tip: Decodable, Hashable {
     let id: Int
     let title: String
     let summary: String
--- DDOBAK/DDOBAK/Features/Home/UI/TipCardView.swift
+++ DDOBAK/DDOBAK/Features/Home/UI/TipCardView.swift
@@ -7,12 +7,90 @@
 
 import SwiftUI
 
-struct TipCardView: View {
+struct TipCardView: View, Equatable {
+    
+    private let viewData: Tip
+    private var action: (((Int)) -> Void)?
+    
+    init(viewData: Tip) {
+        self.viewData = viewData
+    }
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack(alignment: .leading, spacing: 8) {
+            
+            tags
+            
+            title
+            
+            summuary
+        }
+        .frame(maxWidth: .infinity, alignment: .leading)
+        .padding(.vertical, 12)
+        .padding(.horizontal, 20)
+        .background {
+            RoundedRectangle(cornerRadius: 12)
+                .foregroundStyle(.mainWhite)
+        }
+        .onTapGesture {
+            action?(viewData.id)
+        }
+    }
+    
+    static func == (lhs: TipCardView, rhs: TipCardView) -> Bool {
+        return lhs.viewData == rhs.viewData
+    }
+}
+
+// MARK: - Views
+extension TipCardView {
+    var tags: some View {
+        HStack(spacing: 8) {
+            ForEach(viewData.tags, id: \.self) { tagTitle in
+                DdobakTag(
+                    viewData: .init(
+                        title: .makeAttributedString(text: tagTitle, baseFont: .caption2_m12),
+                        titleColor: .mainBlue,
+                        backgroundColor: .gray,
+                        borderColor: .none
+                    )
+                )
+            }
+        }
+    }
+    
+    var title: some View {
+        Text(viewData.title)
+            .font(.ddobak(.body1_sb16))
+            .foregroundStyle(.mainBlack)
+            .lineLimit(1)
+    }
+    
+    var summuary: some View {
+        Text(viewData.summary)
+            .font(.ddobak(.caption2_m12))
+            .foregroundStyle(.gray6)
+            .lineLimit(1)
+    }
+}
+
+// MARK: - Handlers
+extension TipCardView {
+    func onCardViewTap(action: @escaping ((Int)) -> Void) -> Self {
+        var newSelf = self
+        newSelf.action = action
+        return newSelf
     }
 }
 
 #Preview {
-    TipCardView()
+    TipCardView(
+        viewData: .init(
+            id: 1,
+            title: "TipCardView",
+            summary: "This is TipCadView",
+            tags: ["tips, swiftui"],
+            url: ""
+        )
+    )
 }
